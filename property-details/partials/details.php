5f49e5404dbbdd61bb95948040cea49120ec9e33
thebrandstudio <allan@bstt.mx>
2022-10-25 13:57:55 -0500

    Update details.php

--- property-details/partials/details.php
+++ property-details/partials/details.php
@@ -14,33 +14,33 @@ $property_type = houzez_taxonomy_simple('property_type');
 $garage_size = houzez_get_listing_data('property_garage_size');
 $additional_features = get_post_meta( get_the_ID(), 'additional_features', true);
 ?>
-<div class="detail-wrap">
+<div id="detalles" class="detail-wrap">
 	<ul class="<?php echo houzez_option('prop_details_cols', 'list-2-cols'); ?> list-unstyled">
 		<?php
         if( !empty( $prop_id ) && $hide_fields['prop_id'] != 1 ) {
             echo '<li>
-	                <strong>'.houzez_option('spl_prop_id', 'Property ID').':</strong> 
+	                <strong>'.houzez_option('spl_prop_id', 'Property ID').':</strong>
 	                <span>'.houzez_propperty_id_prefix($prop_id).'</span>
                 </li>';
         }
 
         if( $prop_price != "" && $hide_fields['sale_rent_price'] != 1 ) {
             echo '<li>
-	                <strong>'.houzez_option('spl_price', 'Price'). ':</strong> 
+	                <strong>'.houzez_option('spl_price', 'Price'). ':</strong>
 	                <span>'.houzez_listing_price().'</span>
                 </li>';
         }
 
         if( !empty( $prop_size ) && $hide_fields['area_size'] != 1 ) {
             echo '<li>
-	                <strong>'.houzez_option('spl_prop_size', 'Property Size'). ':</strong> 
+	                <strong>'.houzez_option('spl_prop_size', 'Property Size'). ':</strong>
 	                <span>'.houzez_property_size( 'after' ).'</span>
                 </li>';
         }
 
         if( !empty( $land_area ) && $hide_fields['land_area'] != 1 ) {
             echo '<li>
-	                <strong>'.houzez_option('spl_land', 'Land Area'). ':</strong> 
+	                <strong>'.houzez_option('spl_land', 'Land Area'). ':</strong>
 	                <span>'.houzez_property_land_area( 'after' ).'</span>
                 </li>';
         }
@@ -48,7 +48,7 @@ $additional_features = get_post_meta( get_the_ID(), 'additional_features', true)
             $bedrooms_label = ($bedrooms > 1 ) ? houzez_option('spl_bedrooms', 'Bedrooms') : houzez_option('spl_bedroom', 'Bedroom');
 
             echo '<li>
-	                <strong>'.esc_attr($bedrooms_label).':</strong> 
+	                <strong>'.esc_attr($bedrooms_label).':</strong>
 	                <span>'.esc_attr( $bedrooms ).'</span>
                 </li>';
         }
@@ -56,7 +56,7 @@ $additional_features = get_post_meta( get_the_ID(), 'additional_features', true)
             $rooms_label = ($rooms > 1 ) ? houzez_option('spl_rooms', 'Rooms') : houzez_option('spl_room', 'Room');
 
             echo '<li>
-                    <strong>'.esc_attr($rooms_label).':</strong> 
+                    <strong>'.esc_attr($rooms_label).':</strong>
                     <span>'.esc_attr( $rooms ).'</span>
                 </li>';
         }
@@ -64,7 +64,7 @@ $additional_features = get_post_meta( get_the_ID(), 'additional_features', true)
 
             $bath_label = ($bathrooms > 1 ) ? houzez_option('spl_bathrooms', 'Bathrooms') : houzez_option('spl_bathroom', 'Bathroom');
             echo '<li>
-	                <strong>'.esc_attr($bath_label).':</strong> 
+	                <strong>'.esc_attr($bath_label).':</strong>
 	                <span>'.esc_attr( $bathrooms ).'</span>
                 </li>';
         }
@@ -72,38 +72,38 @@ $additional_features = get_post_meta( get_the_ID(), 'additional_features', true)
 
             $garage_label = ($garage > 1 ) ? houzez_option('spl_garages', 'Garages') : houzez_option('spl_garage', 'Garage');
             echo '<li>
-	                <strong>'.esc_attr($garage_label).':</strong> 
+	                <strong>'.esc_attr($garage_label).':</strong>
 	                <span>'.esc_attr( $garage ).'</span>
                 </li>';
         }
         if( !empty( $garage_size ) && $hide_fields['garages'] != 1 ) {
             echo '<li>
-	                <strong>'.houzez_option('spl_garage_size', 'Garage Size').':</strong> 
+	                <strong>'.houzez_option('spl_garage_size', 'Garage Size').':</strong>
 	                <span>'.esc_attr( $garage_size ).'</span>
                 </li>';
         }
         if( !empty( $year_built ) && $hide_fields['year_built'] != 1 ) {
             echo '<li>
-	                <strong>'.houzez_option('spl_year_built', 'Year Built').':</strong> 
+	                <strong>'.houzez_option('spl_year_built', 'Year Built').':</strong>
 	                <span>'.esc_attr( $year_built ).'</span>
                 </li>';
         }
         if( !empty( $property_type ) && ($hide_fields['prop_type']) != 1 ) {
             echo '<li class="prop_type">
-	                <strong>'.houzez_option('spl_prop_type', 'Property Type').':</strong> 
+	                <strong>'.houzez_option('spl_prop_type', 'Property Type').':</strong>
 	                <span>'.esc_attr( $property_type ).'</span>
                 </li>';
         }
         if( !empty( $property_status ) && ($hide_fields['prop_status']) != 1 ) {
             echo '<li class="prop_status">
-	                <strong>'.houzez_option('spl_prop_status', 'Property Status').':</strong> 
+	                <strong>'.houzez_option('spl_prop_status', 'Property Status').':</strong>
 	                <span>'.esc_attr( $property_status ).'</span>
                 </li>';
         }
 
         //Custom Fields
         if(class_exists('Houzez_Fields_Builder')) {
-        $fields_array = Houzez_Fields_Builder::get_form_fields(); 
+        $fields_array = Houzez_Fields_Builder::get_form_fields();
 
             if(!empty($fields_array)) {
                 foreach ( $fields_array as $value ) {
@@ -118,7 +118,7 @@ $additional_features = get_post_meta( get_the_ID(), 'additional_features', true)
                     $data_value = get_post_meta( get_the_ID(), 'fave_'.$value->field_id, $meta_type );
                     $field_title = $value->label;
                     $field_id = houzez_clean_20($value->field_id);
-                    
+
                     $field_title = houzez_wpml_translate_single_string($field_title);
 
                     if( $meta_type == true ) {
@@ -131,14 +131,14 @@ $additional_features = get_post_meta( get_the_ID(), 'additional_features', true)
 
                         if(!empty($data_value) && $hide_fields[$field_id] != 1) {
                             echo '<li class="'.esc_attr($field_id).'"><strong>'.esc_attr($field_title).':</strong> <span><a href="'.esc_url($data_value).'" target="_blank">'.esc_attr( $data_value ).'</a></span></li>';
-                        } 
+                        }
 
                     } else {
                         if(!empty($data_value) && $hide_fields[$field_id] != 1) {
                             echo '<li class="'.esc_attr($field_id).'"><strong>'.esc_attr($field_title).':</strong> <span>'.esc_attr( $data_value ).'</span></li>';
-                        }    
+                        }
                     }
-                    
+
                 }
             }
         }
@@ -160,5 +160,5 @@ $additional_features = get_post_meta( get_the_ID(), 'additional_features', true)
             echo '<li><strong>'.esc_attr( $feature_title ).':</strong> <span>'.esc_attr( $feature_value ).'</span></li>';
         endforeach;
         ?>
-	</ul>	
+	</ul>
 <?php } ?>
