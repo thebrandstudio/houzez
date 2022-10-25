08ecb0f07f1234daa7c687d5e031cf3e428d25da
thebrandstudio <allan@bstt.mx>
2022-10-25 13:50:18 -0500

    Update detail.php

--- property-details/detail.php
+++ property-details/detail.php
@@ -1,4 +1,5 @@
 <?php global $hide_fields; ?>
+PRUEBA
 <div class="property-detail-wrap property-section-wrap" id="property-detail-wrap">
 	<div class="block-wrap">
 		<div class="block-title-wrap d-flex justify-content-between align-items-center">
@@ -8,8 +9,7 @@
 			<?php } ?>
 		</div><!-- block-title-wrap -->
 		<div class="block-content-wrap">
-			<?php get_template_part('property-details/partials/details'); ?> 
+			<?php get_template_part('property-details/partials/details'); ?>
 		</div><!-- block-content-wrap -->
 	</div><!-- block-wrap -->
 </div><!-- property-detail-wrap -->
-
